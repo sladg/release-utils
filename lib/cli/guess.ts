b42e2399741c089a935915e0b102b166cdc2cfe1
Jan <jan@ssoukup.com>
2023-08-17 10:11:17 +0200

    💎 style(eslint): implemented eslint and unified configuration

--- lib/cli/guess.ts
+++ lib/cli/guess.ts
@@ -1,23 +1,30 @@
 import { bumpCalculator, bumpMapping, isValidTag } from '../utils'
 
 interface Props {
-	tagPrefix: string
-	latestVersion: string
-	commitMessage: string
+  tagPrefix: string
+  latestVersion: string
+  commitMessage: string
 }
 
-export const guessHandler = async ({ latestVersion, tagPrefix, commitMessage }: Props) => {
-	if (!isValidTag(latestVersion, tagPrefix)) {
-		throw new Error(`Invalid version found - ${latestVersion}!`)
-	}
+export const guessHandler = async ({
+  latestVersion,
+  tagPrefix,
+  commitMessage,
+}: Props) => {
+  if (!isValidTag(latestVersion, tagPrefix)) {
+    throw new Error(`Invalid version found - ${latestVersion}!`)
+  }
 
-	const match = bumpMapping.find(({ test }) => commitMessage.match(test))
-	if (!match) {
-		throw new Error('No mapping for for suplied commit message.')
-	}
+  const match = bumpMapping.find(({ test }) => commitMessage.match(test))
+  if (!match) {
+    throw new Error('No mapping for for suplied commit message.')
+  }
 
-	const nextTag = bumpCalculator(latestVersion.replace(tagPrefix, ''), match?.bump)
-	const nextTagWithPrefix = tagPrefix + nextTag
+  const nextTag = bumpCalculator(
+    latestVersion.replace(tagPrefix, ''),
+    match?.bump,
+  )
+  const nextTagWithPrefix = tagPrefix + nextTag
 
-	console.log(nextTagWithPrefix)
+  console.log(nextTagWithPrefix)
 }
